f207dcda705025f01a5baf5b90d068c1386b434d
Anna-Ndour <annandour78@gmail.com>
2026-05-16 21:29:15 +0000

    Fix:Makhemout

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ socketServer(io);
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "https://dressup-frontend.vercel.app", "https://dressup.vercel.app", "https://dressup-two.vercel.app"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://dressup-two.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
